1caacfd921a1f92096d35953f76d4662d58e6064
anuraghakeem97 <anurag.hakeem@artemismarket.io>
2022-11-15 18:25:16 +0530

    updated work experience

--- src/Data/Experience/index.js
+++ src/Data/Experience/index.js
@@ -1,84 +1,94 @@
-import Images from "../../Images"
+import Images from "../../Images";
 
-const {EducationDefaultPreviewImage} = Images
+const { EducationDefaultPreviewImage } = Images;
 
 const Experience = [
-    {
-        Title: 'Creative Technologist',
-        Subtitle: 'Adtomica',
-        Year: 'Nov 2020 - Present',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            'Technology Lead at Adtomica globally and Team lead at Adtomica India',
-            "Project Manager for Modicare Global's frontend UI development of 150+ screens",
-            "UX Strategist and UI design lead for Colorbar India's frontend revamp of 100+ screens.",
-            "Project Lead for Adtomica's first NFT collection: Cryptomica on Opensea for integration with Adtomcia's Metaverse",
-            "Designed, developed and deployed 6 websites for glabal brands on Wordpress.l website received honor nomination from Awwwards",
-            "Designed, developed and deployed 5 websites for global brands on Wix",
-            "Project Manager for 4 F&B Singapore brands for social media and online reputation management",
-            "Created 13 Augmented Reality filters for Fb and IG for global brands, some with l0k+ opens",
-            "Created videos and static content for 9 brands for social, eDM, ppt decks, print and digital displays(in-store screens)",
-            "Created a chatbot with dialogflow which is connected to Fb and website",
-        ],
-    },
-    {
-        Title: 'Associate L1 : Experience Technology',
-        Subtitle: 'Publicis Sapient',
-        Year: 'Jul 2019 - Oct 2020',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            "Worked as a UI developer for Altria and developed & maintained multiple components for their existing codebase",
-            "Was a finalist in Sapient's national level hackathon where the team developed an Ecommerce website with the functionality of visual search with camera, recommendation system based on user personalization and augmented reality",
-            "Speaker at Publicis Sapient's Global Summit 2020",
-        ],
-    },
-    {
-        Title: 'Junior Associate : Experience Technology',
-        Subtitle: 'Publicis Sapient',
-        Year: 'Feb 2019 - Jun 2019',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            "Completed a 2 month shadow project for one of the shopping pages for Lowe's in Reactjs. Used Redux for state management for fucntions such filtering by category, ordering by price etc",
-            "Created an end-to-end website for helping HR to track. record and improve their training programs for their employees. The website's front was made in both vanilla js and with reactjs/red",
-            "Completed 2 weeks advanced level training in Reactjs/Redux",
-            "Completed 3 months of training in web development including html/css/JavaScript/ Nodejs/mongodb",
-        ],
-    },
-    {
-        Title: 'Summer Intern : Visualizer',
-        Subtitle: 'Zeno Group',
-        Year: 'Jun 2018 - Aug 2018',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            "Worked for 3 clients in India and 1 client in the US",
-            "Worked on creatives incuding static graphics, cinemagraphs, videos, GDN banners & vox-pop videos for brands like Netflix APAC & US, Smartron, Vuclip and Droom",
-            "Designed multiple eDMs for Zeno Group helping them bag 3rd Best Agency to work for award in the Asia-Pacific Network according to the Holmes Report 2018",
-        ],
-    },
-    {
-        Title: 'Summer Intern : Graphic Design',
-        Subtitle: 'Edelman',
-        Year: 'Jul 2017 - Aug 2017',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            "Worked for 2 global clients and 1 client in India",
-            "Worked on creatives incuding static graphics, motion videos & cinemagraphs for brands like HP, General Electric and Mtv",
-        ],
-    },
-    {
-        Title: 'Summer Intern : Social Media Trainee',
-        Subtitle: 'Pure Design Solution',
-        Year: 'Jun 2016 - Aug 2016',
-        Image: EducationDefaultPreviewImage,
-        Content:[
-            "Worked for 5 different clients in a period of 2 months",
-            "Executed multiple campaigns on Facebook for LiveNutriFit",
-            "Wrote and published press releases for Orane on various online platfor",
-            "Wrote SEO rich biogs for Unocal, LiveNutriFit and Orane",
-            "Optimized websites for ClassHop and lndicolour with SEO rich content",
-        ],
-    },
+  {
+    Title: "Product Manager & Web3 Creative Technologist",
+    Subtitle: "Artemis",
+    Year: "March 2022 - Present",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      " Worked closely with stakeholders, analytics and marketing teams to increase the customer retention on the website by 50% by designing and developing the new design and user journey with the PO,  leading the design team and  development team",
+      " Designed and led the development team to add features to pivot the product strategy from only Solana based marketplace to cross-chain based marketplace which led to an increase in the monthly active users by about 200%",
+      " Designed and led the development team to increase the number of connected wallets (customer accounts) by introducing social interaction features with other customers. Number of connected wallets increased by about 45% in 1 quarter",
+      " Identified gaps in the product sales funnel using analytics reports. Created a new go-to-market strategy & product requirement document to pivot the product theme from a NFT marketplace to NFT aggregator hub. Was the design and development lead in the project. This increased sales conversion from 12% to 43% in 1 quarter",
+      " To improve sales further, improved user journey by adding visual data analytics to help customers make a purchase and added an add to cart feature to save gas fee for customers. This improved average session duration on product page by 40% and total increased up to 61%",
+      " Created go-to-market strategy and product requirement document to a new NFT project backed by in-real-life physical trading cards",
+    ],
+  },
+  {
+    Title: "Creative Technologist",
+    Subtitle: "Adtomica",
+    Year: "Nov 2020 - Feb 2022",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+        " India office operations lead and global technology lead",
+        " Team head-count of India office increased by 450% under his leadership",
+        " Employee retention for 10+ months increased from 20% to 88.9%",
+        " Conversion of trainee/intern to full-time employee increased from 0% to 75%",
+        " Project Lead for 8 global brands and technology lead on all project. Revenue  generated from the projects was over 6-figure SGD",
+        " Implemented and trained team members on technology products such AR filters, chatbots, websites made on low code platforms, CRM and emailer platforms, google analytics. These products generated revenue of about 5-figure SGD",
+        " Many of the above technology products led to increase in the overall project scope and budget by upto 100% and more",
+    ],
+  },
+  {
+    Title: "Associate L1 : Experience Technology",
+    Subtitle: "Publicis Sapient",
+    Year: "Jul 2019 - Oct 2020",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      "Worked as a UI developer for Altria and developed & maintained multiple components for their existing codebase",
+      "Was a finalist in Sapient's national level hackathon where the team developed an Ecommerce website with the functionality of visual search with camera, recommendation system based on user personalization and augmented reality",
+      "Speaker at Publicis Sapient's Global Summit 2020",
+    ],
+  },
+  {
+    Title: "Junior Associate : Experience Technology",
+    Subtitle: "Publicis Sapient",
+    Year: "Feb 2019 - Jun 2019",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      "Completed a 2 month shadow project for one of the shopping pages for Lowe's in Reactjs. Used Redux for state management for fucntions such filtering by category, ordering by price etc",
+      "Created an end-to-end website for helping HR to track. record and improve their training programs for their employees. The website's front was made in both vanilla js and with reactjs/red",
+      "Completed 2 weeks advanced level training in Reactjs/Redux",
+      "Completed 3 months of training in web development including html/css/JavaScript/ Nodejs/mongodb",
+    ],
+  },
+  {
+    Title: "Summer Intern : Visualizer",
+    Subtitle: "Zeno Group",
+    Year: "Jun 2018 - Aug 2018",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      "Worked for 3 clients in India and 1 client in the US",
+      "Worked on creatives incuding static graphics, cinemagraphs, videos, GDN banners & vox-pop videos for brands like Netflix APAC & US, Smartron, Vuclip and Droom",
+      "Designed multiple eDMs for Zeno Group helping them bag 3rd Best Agency to work for award in the Asia-Pacific Network according to the Holmes Report 2018",
+    ],
+  },
+  {
+    Title: "Summer Intern : Graphic Design",
+    Subtitle: "Edelman",
+    Year: "Jul 2017 - Aug 2017",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      "Worked for 2 global clients and 1 client in India",
+      "Worked on creatives incuding static graphics, motion videos & cinemagraphs for brands like HP, General Electric and Mtv",
+    ],
+  },
+  {
+    Title: "Summer Intern : Social Media Trainee",
+    Subtitle: "Pure Design Solution",
+    Year: "Jun 2016 - Aug 2016",
+    Image: EducationDefaultPreviewImage,
+    Content: [
+      "Worked for 5 different clients in a period of 2 months",
+      "Executed multiple campaigns on Facebook for LiveNutriFit",
+      "Wrote and published press releases for Orane on various online platfor",
+      "Wrote SEO rich biogs for Unocal, LiveNutriFit and Orane",
+      "Optimized websites for ClassHop and lndicolour with SEO rich content",
+    ],
+  },
+];
 
-]
-
-export default Experience
+export default Experience;
